e50abc5e7e51009bb5e771b201d727cf3af2c077
ftelnov <fntelnovfn@gmail.com>
2019-05-14 12:01:49 +0300

    Делаем restful

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ DB = SQLAlchemy(app)
 # класс пользователя в базе данных
 class User(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)  # уникальный идентификатор пользователя
-    nickname = DB.Column(DB.String(120), unique=True, nullable=False)  # имя пользователя
+    login = DB.Column(DB.String(120), unique=True, nullable=False)  # имя пользователя
     password = DB.Column(DB.String(120), unique=False, nullable=False)  # пароль пользователя
     token = DB.Column(DB.String, unique=True, nullable=False)  # токен пользователя
     admin = DB.Column(DB.Boolean)  # флаг аккаунта
@@ -65,8 +65,8 @@ def users_get():
     return jsonify(json_list=result)
 
 
-@app.route('/api/auth', methods=['POST'])
-def users_auth():
+@app.route('/api/reg', methods=['POST'])
+def users_reg():
     parser = reqparse.RequestParser()
     parser.add_argument('login')
     parser.add_argument('password')
@@ -83,15 +83,93 @@ def users_auth():
     return jsonify({'Result': 'User successfully created!', 'token': user.token})
 
 
+@app.route('/api/auth', methods=['POST'])
+def users_auth():
+    parser = reqparse.RequestParser()
+    parser.add_argument('login')
+    parser.add_argument('password')
+    result = parser.parse_args()
+    if not result.login or not result.password:
+        return jsonify({'Result': 'One of requirement parameters is missing!'})
+    user = DB.session.query(User).filter_by(login=result.login, password=result.password).first()
+    if not user:
+        return jsonify({'Result': 'There are no such user!'})
+    return jsonify({'Result': 'Auth successfully', 'token': user.token})
+
+
 @app.route('/api/task', methods=['GET', 'POST'])
 def get_all_tasks():
     if request.method == 'GET':
-        token = request.headers.get('token')
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Result': 'One of requirement parameters is missing!'})
         user = DB.session.query(User).filter_by(token=token).first()
         if not user:
             return jsonify({'Result': 'Such user does not exist!'})
         tasks = DB.session.query(Task).filter_by(user_id=user.id).all()
-        t
+        return jsonify(json_list=tasks)
+
+    elif request.method == 'POST':
+        parser = reqparse.RequestParser()
+        parser.add_argument('token')
+        parser.add_argument('text')
+        result = parser.parse_args()
+        if not result.token or not result.text:
+            return jsonify({'Result': 'One of requirement parameters is missing!'})
+        user = DB.session.query(User).filter_by(token=result.token).first()
+        if not user:
+            return jsonify({'Result': 'There are no such user!'})
+        task = Task(text=result.text, user_id=user.id)
+        try:
+            DB.session.add(task)
+            DB.session.commit()
+        except Exception as exc:
+            return jsonify({'Result': 'This task already exist!'})
+        return jsonify({'Result': 'Added successfully'})
+
+
+@app.route('/api/task/id', methods=['GET', 'PUT', 'DELETE'])
+def get_task():
+    if request.method == 'GET':
+        task_id = request.args.get('id')
+        token = request.args.get('token')
+        if not token or not task_id:
+            return jsonify({'Result': 'One of requirement parameters is missing!'})
+        user = DB.session.query(User).filter_by(token=token).first()
+        if not user:
+            return jsonify({'Result': 'Such user does not exist!'})
+        task = DB.session.query(Task).filter_by(user_id=user.id, id=int(task_id)).all()
+        return jsonify(json_list=task)
+
+    elif request.method == 'PUT':
+        task_id = request.args.get('id')
+        token = request.args.get('token')
+        text = request.args.get('text')
+        if not token or not task_id or not text:
+            return jsonify({'Result': 'One of requirement parameters is missing!'})
+        user = DB.session.query(User).filter_by(token=token).first()
+        if not user:
+            return jsonify({'Result': 'Such user does not exist!'})
+        task = DB.session.query(Task).filter_by(user_id=user.id, id=int(task_id)).first()
+        if not task:
+            return jsonify({'Result': 'There are no such task!'})
+        task.text = text
+        DB.session.commit()
+        return jsonify({'Result': 'Successfully!'})
+
+    elif request.method == 'DELETE':
+        task_id = request.args.get('id')
+        token = request.args.get('token')
+        if not task_id or not token:
+            return jsonify({'Result': 'One of requirement parameters is missing!'})
+        user = DB.session.query(User).filter_by(token=token).first()
+        if not user:
+            return jsonify({'Result': 'Such user does not exist!'})
+        task = DB.session.query(Task).filter_by(user_id=user.id, id=int(task_id)).first()
+        if not task:
+            return jsonify({'Result': 'There are no such task!'})
+        DB.session.delete(task)
+        return jsonify({'Result': 'Task successfully deleted!'})
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -3,5 +3,14 @@ from settings import build_url
 
 
 def test_auth(login, password):
-    result = requests.post(build_url('/api/auth'), data={'login': login, 'password': password}).json()
+    result = requests.post(build_url('/api/auth'), data={'login': login, 'password': password})
+    return result.text
+
+
+def test_tasks(user_token):
+    result = requests.get(build_url('/api/task'), params={'token': user_token}).json()
     return result
+
+
+if __name__ == '__main__':
+    print(test_tasks('1'))
